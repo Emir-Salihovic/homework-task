e7ba5286f36158cfcb939850b171ebef1cf482ee
Emir Salihovic <emir.home.11@gmail.com>
2024-07-02 18:58:56 +0200

    build(vercel): removed dev tools, installed husky

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -21,7 +20,6 @@ ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
         <ToastContainer position="top-right" />
         <QueryClientProvider client={queryClient}>
-            <ReactQueryDevtools initialIsOpen={false} />
             <App />
         </QueryClientProvider>
     </React.StrictMode>
